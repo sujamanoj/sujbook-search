6abbfff70873dcba26a89c0290f888c47eadea0d
sujamanoj <suja@masstv.app>
2022-07-31 10:43:11 -0700

    3up server.js

--- server/server.js
+++ server/server.js
@@ -26,15 +26,15 @@ async function startApolloServer() {
   const PORT = process.env.PORT || 3001;
 
   app.use(express.urlencoded({ extended: true }));
-  app.use(express.json()); 
+  app.use(express.json());
 
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
+    app.use(express.static("client/build"));
   }
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 
   db.once("open", () => {
